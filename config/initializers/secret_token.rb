09cbf3650d6f09bbaaa86f04c0b6a8e83c51e883
Sakti Reysaputra <reysaputrasakti@gmail.com>
2014-01-22 10:29:06 +0100

    rails first step

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloGames::Application.config.secret_key_base = '082b3c0f94f5fcc8bbb1ab48033c694f6a9fe1ff2767772ad7ae49640fe30c004b907f6786ce991b4031c167f66305362648b0e56cc7c94ea073303f5e03b110'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HelloGames::Application.config.session_store :cookie_store, key: '_HelloGames_session'
