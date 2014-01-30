d52d983c659eda3b1b6b7f6d15c44b76e857fa56
Sakti Reysaputra <reysaputrasakti@gmail.com>
2014-01-30 17:42:35 +0100

    Landing page Beta v 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ HelloGames::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to :'HelloGames#landpage'
+  root to :'users#index'
 end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
